5a0c1fd89b9c6b7752addea3fdd208d5767439e0
Nenad Caklovic <nenad.caklovic@avl.com>
2015-11-11 18:20:48 +0100

    Added project files

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "main.h"
+#include "rc.h"
+
+int SizeDialog::IDD(){
+	return IDD_SIZE; 
+}
+
+bool SizeDialog::OnInitDialog(){
+	return true;
+}
+
+bool SizeDialog::OnOK(){
+	return true;
+}
+
+
+void MainWindow::OnPaint(HDC hdc){
+}
+
+void MainWindow::OnCommand(int id){
+	switch(id){
+		case ID_SIZE:
+			break;
+		case ID_COLOR:
+			break;
+		case ID_EXIT: 
+			DestroyWindow(*this); 
+			break;
+	}
+}
+
+void MainWindow::OnDestroy(){
+	::PostQuitMessage(0);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
+{
+	Application app;
+	MainWindow wnd;	
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP", 
+		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
+	return app.Run();
+}
---
+++ rc.h
@@ -0,0 +1,10 @@
+#define IDM_MAIN	101
+#define ID_SIZE		102
+#define ID_COLOR	103
+#define ID_EXIT		104
+
+#define IDD_SIZE     105
+#define IDC_EDIT1    106
+#define IDC_EDIT2    107
+
+
