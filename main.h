0b2b0106e6f2184c23fd6912251041080c3584f2
Ana-Marija Kostanjčar <anamarija.kostanjcar@gmail.com>
2017-11-20 20:23:15 +0100

    rijesena vjezba

--- main.h
+++ main.h
@@ -8,6 +8,8 @@ protected:
 	int IDD();
 	bool OnInitDialog();
 	bool OnOK();
+public:
+	int x, y;
 };
 
 class MainWindow : public Window {
@@ -15,4 +17,5 @@ protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+
 };
