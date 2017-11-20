ccd5bdfb57d5473e89c747451712b374904718c0
Petar Domitrović <domitrovicpetar@gmail.com>
2017-11-20 20:12:47 +0100

    Gotova šahovnica. v6a

--- main.cpp
+++ main.cpp
@@ -1,31 +1,78 @@
 #include "main.h"
 #include "rc.h"
 
+int x = 10; 
+int y = 10;
+COLORREF color = RGB(0, 0, 0);
+
 int SizeDialog::IDD(){
 	return IDD_SIZE; 
 }
 
 bool SizeDialog::OnInitDialog(){
+	SetInt(IDC_EDIT1, x);
+	SetInt(IDC_EDIT2, y);
 	return true;
 }
 
 bool SizeDialog::OnOK(){
+	x = GetInt(IDC_EDIT1);
+	y = GetInt(IDC_EDIT2);
 	return true;
 }
 
 
 void MainWindow::OnPaint(HDC hdc){
+	HBRUSH brush = CreateSolidBrush(color);
+	RECT cr;
+	GetClientRect(*this, &cr);
+	POINT p;
+	SetMapMode(hdc, MM_ANISOTROPIC);
+	SetViewportExtEx(hdc, cr.right, cr.bottom, 0);
+	SetWindowExtEx(hdc, x, y, 0);
+	
+	for (int i = 0; i < x; ++i) {
+		for (int j = i&1; j < y; j += 2) {
+			RECT r = { i, j, i + 1, j + 1 };
+			FillRect(hdc, &r, brush);
+		}
+	}
 }
 
 void MainWindow::OnCommand(int id){
-	switch(id){
-		case ID_SIZE:
-			break;
-		case ID_COLOR:
-			break;
-		case ID_EXIT: 
-			DestroyWindow(*this); 
-			break;
+	switch (id) {
+	case ID_SIZE:
+	{
+		SizeDialog size;
+		size.x = x;
+		size.y = y;
+		if (size.DoModal(0, *this) == IDOK) {
+			x = size.x;
+			y = size.y;
+			InvalidateRect(*this, NULL, true);
+		}
+		break;
+	}
+		
+	case ID_COLOR:
+		{
+			COLORREF custCols[16] = { 0 };
+			CHOOSECOLOR cc;
+			ZeroMemory(&cc, sizeof cc);
+			cc.lStructSize = sizeof cc;
+			cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+			cc.hwndOwner = *this;
+			cc.lpCustColors = custCols;
+			cc.rgbResult = color;
+			if (ChooseColor(&cc)) {
+				color = cc.rgbResult;
+				InvalidateRect(*this, NULL, true);
+			}
+		}			
+		break;
+	case ID_EXIT: 
+		DestroyWindow(*this); 
+		break;
 	}
 }
 
