76637bb8cdf9dca0e87b60a1b1db383d04eafcd7
tleva <tleva@DESKTOP-J8PKM7S>
2019-12-07 16:04:18 +0100

    Added x,y

--- main.h
+++ main.h
@@ -8,6 +8,9 @@ protected:
 	int IDD();
 	bool OnInitDialog();
 	bool OnOK();
+public:
+	int x;
+	int y;
 };
 
 class MainWindow : public Window {
@@ -15,4 +18,9 @@ protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
 	void OnDestroy();
+public:
+	COLORREF c = RGB(240, 230, 140);
+	int x = 10;
+	int y = 10;
+
 };
