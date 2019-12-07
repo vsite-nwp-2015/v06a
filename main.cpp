b3d6a450e4e95d9135a72ae7afce4bce89d2412b
tleva <tleva@DESKTOP-J8PKM7S>
2019-12-07 16:01:11 +0100

    Finished

--- main.cpp
+++ main.cpp
@@ -1,43 +1,97 @@
 #include "main.h"
 #include "rc.h"
 
-int SizeDialog::IDD(){
-	return IDD_SIZE; 
+int SizeDialog::IDD() 
+{
+	return IDD_SIZE;
 }
 
-bool SizeDialog::OnInitDialog(){
+bool SizeDialog::OnInitDialog() 
+{
+	SetInt(IDC_EDIT1, x);
+	SetInt(IDC_EDIT2, y);
 	return true;
 }
 
-bool SizeDialog::OnOK(){
+bool SizeDialog::OnOK() 
+{
+	try {
+		x = GetInt(IDC_EDIT1);
+		y = GetInt(IDC_EDIT2);
+	}
+	catch (XCtrl & ex) {
+		MessageBox(*this, "Unesene su krivi podaci!! ","Krivi Podaci!", MB_OK);
+		MessageBox(*this, "Molim unesite brojeve", "Size", MB_OK);
+		return false;
+	}
 	return true;
 }
 
 
-void MainWindow::OnPaint(HDC hdc){
+void MainWindow::OnPaint(HDC hdc) 
+{
+	RECT win;
+	HBRUSH brush = CreateSolidBrush(c);
+	GetClientRect(*this, &win);
+	SetMapMode(hdc, MM_ANISOTROPIC);
+	SetViewportExtEx(hdc, win.right, win.bottom, 0);
+	SetWindowExtEx(hdc, x, y, 0);
+	for (int i = 0; i < x; i++) {
+		for (int j = (i & 1); j < y; j += 2) {
+			RECT r = { i, j, i + 1, j + 1 };
+			FillRect(hdc, &r, brush);
+		}
+	}
+
+	DeleteObject(brush);
 }
 
-void MainWindow::OnCommand(int id){
-	switch(id){
-		case ID_SIZE:
-			break;
-		case ID_COLOR:
-			break;
-		case ID_EXIT: 
-			DestroyWindow(*this); 
-			break;
+void MainWindow::OnCommand(int id) {
+	switch (id) {
+	case ID_SIZE:
+	{
+		SizeDialog nbr;
+		nbr.x = x;
+		nbr.y = y;
+		if (nbr.DoModal(0, *this) == IDOK) {
+			x = nbr.x;
+			y = nbr.y;
+			InvalidateRect(*this, NULL, true);
+
+		}
+		break;
+	}
+	case ID_COLOR:
+	{
+		COLORREF custCols[16] = { 0 };
+		CHOOSECOLOR cc;
+		ZeroMemory(&cc, sizeof cc);
+		cc.lStructSize = sizeof cc;
+		cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+		cc.hwndOwner = *this;
+		cc.lpCustColors = custCols;
+		cc.rgbResult = c;
+		if (ChooseColor(&cc)) {
+			c = cc.rgbResult;
+			InvalidateRect(*this, NULL, true);
+		}
+		break;
+	}
+	case ID_EXIT:
+		DestroyWindow(*this);
+		break;
 	}
 }
 
-void MainWindow::OnDestroy(){
+void MainWindow::OnDestroy() {
 	::PostQuitMessage(0);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	Application app;
-	MainWindow wnd;	
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP", 
-		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));	
+	MainWindow wnd;
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP",
+		(int)LoadMenu(hInstance, MAKEINTRESOURCE(IDM_MAIN)));
 	return app.Run();
 }
