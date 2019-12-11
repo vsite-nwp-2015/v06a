1656610aff4260694efbe54def15c4f8add17430
Dona Pejnović <don@DESKTOP-QROSDFJ>
2019-12-12 00:00:34 +0100

    Gotova vj6 a

--- main.cpp
+++ main.cpp
@@ -6,22 +6,80 @@ int SizeDialog::IDD(){
 }
 
 bool SizeDialog::OnInitDialog(){
+	SetInt(IDC_EDIT1, paramsXY.x);
+	SetInt(IDC_EDIT2, paramsXY.y);
+	return true;
+}
+
+void HandleError(HWND hwnd, int ctrlID, std::string message) {
+	SetFocus(GetDlgItem(hwnd, ctrlID));
+	SendDlgItemMessage(hwnd, ctrlID, EM_SETSEL, 0, -1);
+	MessageBox(hwnd, message.c_str(), "Size", MB_OK);
+}
+
+bool SizeDialog::CheckInput(long& ref, int ctrlID) {
+	try {
+		ref = GetInt(ctrlID);
+		if (ref < 0) {
+			HandleError(*this, ctrlID, "Negative number!");
+			return false;
+		}
+	} catch (XCtrl&) {
+		HandleError(*this, ctrlID, "Not a number!");
+		return false;
+	}
 	return true;
 }
 
 bool SizeDialog::OnOK(){
-	return true;
+	return CheckInput(paramsXY.x,IDC_EDIT1) && CheckInput(paramsXY.y,IDC_EDIT2)	;
 }
 
-
 void MainWindow::OnPaint(HDC hdc){
+	RECT rc;
+	HBRUSH brush = CreateSolidBrush(color);
+	GetClientRect(*this, &rc);
+	SetMapMode(hdc, MM_ANISOTROPIC);
+	SetViewportExtEx(hdc, rc.right, rc.bottom, NULL);
+	SetWindowExtEx(hdc, params.x, params.y, NULL);
+	for(int i=0;i<params.x;++i)
+		for (int j = (i & 1); j < params.y; j += 2) {
+			RECT r = { i,j,i + 1,j + 1 };
+			FillRect(hdc, &r, brush);
+		}
+	//DeleteObject(brush);
+}
+
+void OpenSizeDialog(POINT& params, const HWND parent) {
+	SizeDialog sizeDialog(params);
+	if (sizeDialog.DoModal(0, parent) == IDOK) {
+		params = sizeDialog.GetParamsXY();
+		InvalidateRect(parent, 0, true);
+	}
+}
+
+COLORREF GetColor(HWND parent, COLORREF cur) {
+	COLORREF custCols[16] = { 0 };
+	CHOOSECOLOR cc;
+	ZeroMemory(&cc, sizeof cc);
+	cc.lStructSize = sizeof cc;
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	cc.hwndOwner = parent;
+	cc.lpCustColors = custCols;
+	cc.rgbResult = cur;
+	if (ChooseColor(&cc))
+		cur = cc.rgbResult;
+	return cur;
 }
 
 void MainWindow::OnCommand(int id){
 	switch(id){
 		case ID_SIZE:
+			OpenSizeDialog(params, *this);
 			break;
 		case ID_COLOR:
+			color = GetColor(*this, color);
+			InvalidateRect(*this, NULL, true);
 			break;
 		case ID_EXIT: 
 			DestroyWindow(*this); 
--- main.h
+++ main.h
@@ -4,13 +4,28 @@
 #include "nwpdlg.h"
 
 class SizeDialog : public Dialog {
+private:
+	POINT paramsXY;
+public:
+	SizeDialog(POINT params)
+		:paramsXY(params) {}
+	POINT GetParamsXY() const { return paramsXY; }
 protected:
 	int IDD();
 	bool OnInitDialog();
 	bool OnOK();
+	bool CheckInput(long& ref, int ctrlID);
+
 };
 
 class MainWindow : public Window {
+private:
+	COLORREF color;
+	POINT params;
+public:
+	MainWindow()
+		:params({ 20,20 }), color(RGB(128, 128, 255))
+	{}
 protected:
 	void OnPaint(HDC hdc);
 	void OnCommand(int id);
